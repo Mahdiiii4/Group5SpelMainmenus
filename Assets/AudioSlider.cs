5708cd4b674e657b1a17c77762e5f1a9e5f4c929
Mahdiiii <mhdisy0@gmail.com>
2023-11-15 12:04:23 +0100

    Kod

---
+++ Assets/AudioSlider.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSlider : MonoBehaviour
+{
+    public void Confirm_Button()
+    {
+        public int value = AudioSlider.value;
+        Debug.Log(value);
+    }
+}
