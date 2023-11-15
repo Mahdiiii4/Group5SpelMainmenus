10e43955856c9287d6be8d7a47dc8caac8b7508e
Mahdiiii <mhdisy0@gmail.com>
2023-11-15 14:13:01 +0100

    UI kod

--- Assets/AudioSlider.cs
+++ Assets/AudioSlider.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class AudioSlider : MonoBehaviour
 {
+    int n;
+    public TMP_Text Audio_text;
+    public Slider Slider;
+    
+    void update()
+    {
+        Audio_text.text = Slider.value.ToString();
+        Debug.Log("" + Audio_text.text);
+    }
+    
     public void Confirm_Button()
     {
-        public int value = AudioSlider.value;
-        Debug.Log(value);
     }
 }
